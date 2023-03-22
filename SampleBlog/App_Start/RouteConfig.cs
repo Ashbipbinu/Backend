b4d5e73531ad8c6a59717d64a94cd93ea75cc1f5
DESKTOP-DTME2N6\LENOVO <abinu@thoughtlinetech.com>
2023-03-22 10:50:37 +0530

    First commit ====>Backend Project

---
+++ SampleBlog/App_Start/RouteConfig.cs
@@ -0,0 +1,22 @@
+﻿using SampleBlog.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SampleBlog
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            var namespaces = new [] {typeof(PostController).Namespace};
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute("Home", "", new { controller = "Post", action = "index" }, namespaces);
+            routes.MapRoute("Login", "login", new { controller = "Auth", action = "Login" }, namespaces);
+        }
+    }
+}
